a02ed90da31c6a8333b50d83cfc63bf6e8365039
Mats Lillas <78966461+mlillas@users.noreply.github.com>
2021-10-19 16:45:04 +0300

    Update video.py

--- video.py
+++ video.py
@@ -1,5 +1,7 @@
+# This is an extra script you can run to test the video stream from your IP camera.
+
 import cv2
-cap = cv2.VideoCapture("rtsp://admin:admin123@192.168.1.150:88/videoMain")
+cap = cv2.VideoCapture("rtsp://admin:admin123@192.168.1.150:88/videoMain") # <-- EDIT HERE
 
 ret, frame = cap.read()
 
