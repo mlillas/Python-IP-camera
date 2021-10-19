1fb79e774d1d75d653f22dadb85f8365512f2bc3
Mats Lillas <78966461+mlillas@users.noreply.github.com>
2021-10-19 16:45:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-FTP_url = 'www.kolumbus.fi'
+FTP_url = 'www.default.com'
 
-FTP_username = 'vr4091857'
-FTP_passwd = 
+FTP_username = ''
+FTP_passwd = ''
 
 # Time between image capture and upload to the server. Written in minutes. 
-interval = 1
+interval = 60
 
 # Remember to edit the video capture URL in 'capture.py' for your specific IP camera.
